70a8eced41190ae9115d036a59808e35ab3cbbac
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2018-12-07 11:00:18 -0500

    Moved home to sub component and added routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResultsComponent } from './results/results.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResultsComponent
+    ResultsComponent,
+    HomeComponent
   ],
   imports: [
     AppRoutingModule,
