d0402512c5d5348f2f0f2b0d8b94faffd1b927cd
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2018-12-07 11:17:07 -0500

    Basic card layout init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
@@ -21,7 +24,10 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatChipsModule,
     MatButtonModule,
+    MatGridListModule,
     MatIconModule,
     MatInputModule,
   ],
