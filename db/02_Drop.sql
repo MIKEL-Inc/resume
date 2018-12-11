0b827bbdbc64c7a620e8ef85358288e5998f640e
Chris Bosse <christopher.bosse@mikelinc.com>
2018-12-11 17:30:49 -0500

    Add Database setup scripts

---
+++ db/02_Drop.sql
@@ -0,0 +1,14 @@
+DROP TABLE app_user CASCADE;
+DROP TABLE resume CASCADE;
+DROP TABLE person CASCADE;
+DROP TABLE comment CASCADE;
+DROP TABLE internal_employee_type CASCADE;
+DROP TABLE internal_employee_status CASCADE;
+DROP TABLE degree CASCADE;
+DROP TABLE schooling_level CASCADE;
+DROP TABLE security_clearance CASCADE;
+DROP TABLE resume_source CASCADE;
+DROP TABLE status_of_person CASCADE;
+
+DROP TABLE changelog;
+DROP DATABASE postgres;
