a6d05ff20c41acf85b38a296298e5ed4b01c9848
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2018-12-13 11:04:40 -0500

    GUI design tweaks and SASS updates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResultsComponent } from './results/results.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResultsComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatBadgeModule,
     MatCardModule,
     MatChipsModule,
+    MatDividerModule,
     MatButtonModule,
     MatGridListModule,
     MatIconModule,
     MatInputModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+
+import { SearchComponent } from '../search/search.component';
+
+const RESULTS = [
+  {
+    id: 0,
+    name: 'John Doe',
+    status: 'candidate',
+    clearance: 'Secret',
+    date: '2/12/2017'
+  },
+  {
+    id: 2,
+    name: 'Roger Yuri',
+    status: 'employee',
+    clearance: 'Secret',
+    date: '1/4/2018'
+  },
+  {
+    id: 3,
+    name: 'Stephanie Apple',
+    status: 'former',
+    clearance: 'Top Secret',
+    date: '4/3/2018'
+  },
+];
+
 
 @Component({
   selector: 'app-results',
@@ -7,9 +34,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(SearchComponent) search;
+
+  results = RESULTS;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.search = this.search.keywords;
   }
 
 }
