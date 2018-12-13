b63657bdf1ad056c793686af26d5cf26618efeec
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2018-12-13 11:36:23 -0500

    removed person route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { ResultsComponent } from './results/results.component';
-import { PersonDetailComponent } from './person-detail/person-detail.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full'},
   { path: 'results', component: ResultsComponent },
-  { path: 'detail', component: PersonDetailComponent }
 ];
 
 @NgModule({
