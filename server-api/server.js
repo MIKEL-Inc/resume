bb064b2c47609193531675cc8d66815046aa71f8
Chris Bosse <christopher.bosse@mikelinc.com>
2018-12-13 09:59:00 -0500

    Clickable link for developer

--- server-api/server.js
+++ server-api/server.js
@@ -35,4 +35,4 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
