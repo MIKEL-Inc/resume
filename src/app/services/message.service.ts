63c8e2ddf7407bd21607fc3ef9b104074c2f859f
Chris Bosse <christopher.bosse@mikelinc.com>
2019-01-10 15:05:48 -0500

    Linting - Remove trailing space

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
   messages: string[] = [];
- 
+
   add(message: string) {
     this.messages.push(message);
   }
- 
+
   clear() {
     this.messages = [];
   }
