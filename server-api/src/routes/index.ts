5b87eb6b5633a55a217ddb9c958771dcb999f6ce
Chris Bosse <christopher.bosse@mikelinc.com>
2019-02-07 10:48:28 -0500

    Remove database view
    Closes #19

--- server-api/src/routes/index.ts
+++ server-api/src/routes/index.ts
@@ -1,9 +1,7 @@
 import { Express } from 'express';
 
 import graphql from './graphql';
-import person from './person';
 
 export default (app: Express) =>
   app
-    .use('/graphql', graphql)
-    .use('/person', person);
+    .use('/graphql', graphql);
