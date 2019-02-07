f58ac8fbc91c47f1fba239e35c1b78a1484555fd
Chris Bosse <christopher.bosse@mikelinc.com>
2019-02-07 10:31:24 -0500

    Formatting

--- server-api/src/server.ts
+++ server-api/src/server.ts
@@ -4,21 +4,13 @@ import cors from 'cors';
 import mountRoutes from './routes';
 
 const server = express();
-// console.log({'ourSchema': ourSchema});
+
 mountRoutes(server);
-server
-  .use(cors())  // FIXME: Not needed in Production when all is on same server.
-  ;
+
+server.use(cors()); // FIXME: Not needed in Production when all is on same server.
+
 server.listen(4000);
-/* legitimate use of console.log */ console.log('Running a GraphQL API server at http://localhost:4000/graphql');
-// console.log({'process.env': process.env});
-// console.log({
-//   comment:
-//     'Select Environment Variables \
-//   that concern the database plugin "pg".',
-//   'PGUSER': process.env.PGUSER,
-//   'PGHOST': process.env.PGHOST,
-//   'PGPASSWORD': process.env.PGPASSWORD,
-//   'PGDATABASE': process.env.PGDATABASE,
-//   'PGPORT': process.env.PGPORT
-// });
+
+/* legitimate use of console.log */ console.log(
+  'Running a GraphQL API server at http://localhost:4000/graphql'
+);
