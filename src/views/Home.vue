073f7b363b79b0a01fc1902544f15bdf9383959e
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-02-14 12:31:16 -0500

    Layout cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <HelloWorld />
+  <v-container grid-list-xl fluid>
+    <v-layout row wrap>
+      <v-flex
+        v-for="n in 9"
+        :key="n"
+        xs4
+      >
+        <Card />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import Card from '../components/Card'
 
   export default {
+    data: () => ({
+      //
+    }),
     components: {
-      HelloWorld
+      Card
     }
   }
 </script>
