12d59cbf978b4116f70a1fa8a3fd61fa56dce979
Lance Gundersen <LanceGundersen@users.noreply.github.com>
2019-02-21 11:26:58 -0500

    Provide login view and routing

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import CardGrid from './views/CardGrid.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -10,17 +10,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'grid',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
       props: true,
-      component: CardGrid
+      // route level code-splitting
+      // this generates a separate chunk (home.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ]
 })
